68dc3121fe7a74900dca5d3f92c3e85fbad46155
pfazekas <v-pfazekas@hotels.com>
2020-10-28 17:07:27 +0100

    Introduce Lists

--- src/hu/prgitems/App.java
+++ src/hu/prgitems/App.java
@@ -1,13 +1,15 @@
 package hu.prgitems;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class App {
 
     private static final int SIZE = 100;
-    public static final int BOUND = 99;
+    private static final int BOUND = 99;
 
-    private static int[] numbers;
+    private static List<Integer> numbers;
 
     public static void main(String[] args) {
         numbers = init();
@@ -31,27 +33,21 @@ public class App {
                 " db " + divisor + "-al osztható szám van.");
         int max = maxSelection();
         System.out.println("6. Maximum kiválasztás: A sorozat " + (max + 1) +
-                ". eleme a legnagyobb, értéke: " + numbers[max]);
-        System.out.println("A rendezetlen tömb elemei:");
-        print();
-//        simpleSort();
-        bubbleSort();
-        System.out.println("A rendezett tömb elemei:");
-        print();
+                ". eleme a legnagyobb, értéke: " + numbers.get(max));
     }
 
-    private static int[] init() {
-        int[] numbers = new int[SIZE];
+    private static List<Integer> init() {
+        List<Integer> numbers = new ArrayList<>();
         Random random = new Random();
         for (int i = 0; i < SIZE; i++) {
-            numbers[i] = random.nextInt(BOUND) + 1;
+            numbers.add(random.nextInt(BOUND) + 1);
         }
         return numbers;
     }
 
     private static void print() {
         for (int i = 0; i < SIZE; i++) {
-            System.out.printf("%4d", numbers[i]);
+            System.out.printf("%4d", numbers.get(i));
         }
         System.out.println();
     }
@@ -59,22 +55,22 @@ public class App {
     private static int summation() {
         int sum = 0;
         for (int i = 0; i < SIZE; i++) {
-            sum += numbers[i]; // sum = sum + numbers[i]
+            sum += numbers.get(i); // sum = sum + numbers[i]
         }
         return sum;
     }
 
     private static boolean decision(int divisor) {
         int i = 0;
-        while (i < numbers.length && !(numbers[i] % divisor == 0)) {
+        while (i < numbers.size() && !(numbers.get(i) % divisor == 0)) {
             i++;
         }
-        return i < numbers.length;
+        return i < numbers.size();
     }
 
     private static int selection(int divisor) {
         int i = 0;
-        while (!(numbers[i] % divisor == 0)) {
+        while (!(numbers.get(i) % divisor == 0)) {
             i++;
         }
         return i;
@@ -82,16 +78,16 @@ public class App {
 
     private static int search(int divisor) {
         int i = 0;
-        while (i < numbers.length && !(numbers[i] % divisor == 0)) {
+        while (i < numbers.size() && !(numbers.get(i) % divisor == 0)) {
             i++;
         }
-        return i < numbers.length ? i : -1;
+        return i < numbers.size() ? i : -1;
     }
 
     private static int count(int divisor) {
         int count = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % divisor == 0) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) % divisor == 0) {
                 count++;
             }
         }
@@ -100,35 +96,11 @@ public class App {
 
     private static int maxSelection() {
         int max = 0;
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > numbers[max]) {
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i) > numbers.get(max)) {
                 max = i;
             }
         }
         return max;
-    }
-
-    private static void simpleSort() {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] > numbers[j]) {
-                    int p = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = p;
-                }
-            }
-        }
-    }
-
-    private static void bubbleSort() {
-        for (int i = numbers.length - 1; i > 1; i--) {
-            for (int j = 0; j < i; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    int p = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = p;
-                }
-            }
-        }
     }
 }
